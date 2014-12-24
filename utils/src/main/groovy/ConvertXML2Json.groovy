e8ee2e459da6a7f4840f6b156894da7c9eaa55e8
nikos <nikos@avocarrot.com>
2014-12-24 12:32:56 +0200

    create utils module (adding there the script to convert android xml to json)

---
+++ utils/src/main/groovy/ConvertXML2Json.groovy
@@ -0,0 +1,96 @@
+
+def layout = new XmlParser().parse("utils/src/main/groovy/test.xml")
+
+println  new groovy.json.JsonBuilder( createView(layout) ).toString();
+
+View createView (node) {
+
+    def name = "";
+    def properties = [];
+    def views = [];
+
+    name = node.name().toString();
+
+    node.attributes().each { t ->
+        properties.push( createProperty(t) )
+    }
+
+    node.value().each { t ->
+        views.push( createView(t) );
+    }
+
+    return new View( name, properties, views );
+
+}
+
+Property createProperty (attribute) {
+
+    def name = attribute.key.localPart;
+    def type = "string";
+    def value =  attribute.value;
+
+    switch(name.toUpperCase()) {
+        case "ID":
+        case "LAYOUT_ABOVE":
+        case "LAYOUT_ALIGNBASELINE":
+        case "LAYOUT_ALIGNBOTTOM":
+        case "LAYOUT_ALIGNEND":
+        case "LAYOUT_ALIGNLEFT":
+        case "LAYOUT_ALIGNRIGHT":
+        case "LAYOUT_ALIGNSTART":
+        case "LAYOUT_ALIGNTOP":
+        case "LAYOUT_BELOW":
+        case "LAYOUT_TOENDOF":
+        case "LAYOUT_TOLEFTOF":
+        case "LAYOUT_TORIGHTOF":
+        case "LAYOUT_TOSTARTOF":
+            type = "ref";
+            try {
+                value = value.split("/")[1];
+            } catch (Exception e) {}
+            break;
+        case "BACKGROUND":
+        case "TEXTCOLOR":
+            type = "color";
+            break;
+        case "LAYOUT_WIDTH":
+        case "LAYOUT_HEIGHT":
+        case "PADDING_LEFT":
+        case "PADDING_RIGHT":
+        case "PADDING_TOP":
+        case "PADDING_BOTTOM":
+        case "PADDING":
+        case "TEXTSIZE":
+            type = "dimen";
+            break;
+        case "LAYOUT_ALIGNWITHPARENTIFMISSING":
+        case "LAYOUT_CENTERHORIZONTAL":
+        case "LAYOUT_CENTERINPARENT":
+        case "LAYOUT_CENTERVERTICAL":
+        case "LAYOUT_ALIGNPARENTBOTTOM":
+        case "LAYOUT_ALIGNPARENTEND":
+        case "LAYOUT_ALIGNPARENTLEFT":
+        case "LAYOUT_ALIGNPARENTRIGHT":
+        case "LAYOUT_ALIGNPARENTSTART":
+        case "LAYOUT_ALIGNPARENTTOP":
+            type = "BOOLEAN";
+            break;
+    }
+
+    return new Property( name, type, value );
+
+}
+
+@groovy.transform.Canonical
+@ groovy.transform.ToString(excludes='view')
+class View {
+    String widget
+    List<Property> properties
+    List<View> views
+}
+@groovy.transform.Canonical
+class Property {
+    String name
+    String type
+    String value
+}
